b1f319669f05297a70f7ec3f97293f8309738f74
nikki123240 <70109048+nikki123240@users.noreply.github.com>
2022-07-27 22:39:54 +0530

    Initial Commit

---
+++ lib/screens/ProductOverView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductOverView extends StatelessWidget {
+  const ProductOverView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Products"),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/widgets/Product_item.dart
@@ -0,0 +1,12 @@
+import 'package:ecommapp/controllers/Product_Item_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ProductItem extends GetView<ProductItemController> {
+  const ProductItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
